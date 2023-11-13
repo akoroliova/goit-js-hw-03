2d3bcc90286e4bcd33079829c1134fe66b931fa7
Oleksandra K <a.koroliova@gmail.com>
2023-11-13 17:48:18 +0200

    Complete task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,19 @@
-function checkForSpam(message) {
-  message = message.toLowerCase();
+function filterArray(numbers, value) {
+  let newArray = [];
 
-  if (message.includes("spam") || message.includes("sale")) {
-    return true;
-  } else {
-    return false;
+  for (let i = 0; i < numbers.length; i += 1) {
+    const largerValue = numbers[i];
+
+    if (largerValue > value) {
+      newArray.push(largerValue);
+    }
   }
+
+  return newArray;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
